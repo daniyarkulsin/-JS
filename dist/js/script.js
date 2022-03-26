39e2e35cea411b2cbaf69f10cc0664102135c33f
Danik <daniar.kuslin@gmail.com>
2022-03-26 22:04:08 +0600

    leason58

--- dist/js/script.js
+++ dist/js/script.js
@@ -285,6 +285,8 @@ window.addEventListener('DOMContentLoaded', function() {
         }, 4000);
     }
 
-
+    fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 
 });
